46f31bed674cbf5d7af3e0211600e674bbfa2ec0
Dmitry112233 <dmitr.starovoitov@gmail.com>
2019-03-12 13:58:37 +0300

    Initial commit

---
+++ src/main/java/DemoChatPage.java
@@ -0,0 +1,12 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class DemoChatPage {
+
+    private WebDriver driver;
+
+    public DemoChatPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+}
